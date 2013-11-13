06354d0e17036983963c5a31104e13959c2cc7b2
mauro <mauro@iMac-di-Mauro.local>
2013-11-13 22:54:44 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SocialLibreria1::Application.config.secret_token = '4a14f226a554f0c3afc1eca4678467fb5d849266109993c06c88312f9e050d53d42039fb0be77cb917291ae41dd04a19d3ffe5c9f437c9afab23a79e2317013d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+SocialLibreria1::Application.config.session_store :cookie_store, key: '_Social_libreria1_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# SocialLibreria1::Application.config.session_store :active_record_store
